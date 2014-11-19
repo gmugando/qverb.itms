ccfda873e129548b329f2f0d312626bb22cd553b
Qverb.ITMS <gmugando@gmail.com>
2014-11-19 16:42:59 +0200

    changed RedirectToLocal to RedirectToAction

--- QverbITMS.Web/Controllers/AccountController.cs
+++ QverbITMS.Web/Controllers/AccountController.cs
@@ -37,7 +37,8 @@ namespace QverbITMS.Web.Controllers
         {
             if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password, persistCookie: model.RememberMe))
             {
-                return RedirectToLocal(returnUrl);
+                //return RedirectToLocal(returnUrl);
+                return RedirectToAction("Index", "Home");
             }
 
             // If we got this far, something failed, redisplay form
