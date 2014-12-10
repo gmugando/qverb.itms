a9f1d4f907e0de526d9a658110fd562c0ae2f33a
gmugando@gmail.com <gmugando@gmail.com>
2014-12-10 13:49:54 +0200

    Qverb.Service first iteration

---
+++ QverbITMS.Services/Interfaces/IIncidentService.cs
@@ -0,0 +1,22 @@
+﻿using QverbITMS.Core;
+using QverbITMS.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QverbITMS.Services.Interfaces
+{
+    public interface IIncidentService
+    {
+        Incident GetIncidentById(int incidentId);
+
+        IPagedList<Incident> GetIncidentsByUser(int userId, DateTime? dateFrom, DateTime? dateTo, int pageIndex, int pageSize, bool showHidden = false);
+
+        IList<Incident> GetActiveIncidents();
+
+        void Insert(Incident incident);
+
+        void Update(Incident incident);
+    }
+}
