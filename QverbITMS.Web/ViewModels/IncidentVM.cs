1960a191ce75037ed1c63132e5c9ed27fc06ee1e
gmugando@gmail.com <gmugando@gmail.com>
2014-12-10 13:56:50 +0200

    view models

---
+++ QverbITMS.Web/ViewModels/IncidentVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QverbITMS.Web.ViewModels
+{
+    public class IncidentVM
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Description")]
+        public string Descr { get; set; }
+        [Required]
+        [Display(Name = "Date Logged")]
+        public DateTime LoggedDate { get; set; }
+        [Display(Name = "Logged By")]
+        public string LoggedBy { get; set; }
+        [Required]
+        [Display(Name = "Percentage Complete")]
+        public int PercentageComplete { get; set; }
+        [Display(Name = "Status")]
+        public bool Status { get; set; }
+    }
+}
