bebc31dba4357c40a8a453944cf443ac09cde3f9
gmugando@gmail.com <gmugando@gmail.com>
2014-12-10 16:33:18 +0200

    Add Dashboard viewmodel

---
+++ QverbITMS.Web/ViewModels/DashboardVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QverbITMS.Web.ViewModels
+{
+    public class DashboardVM
+    {
+        public int NewIncidents { get; set; }
+        public int ResolvedIncidents { get; set; }
+        public int UserReg { get; set; }
+
+    }
+}
