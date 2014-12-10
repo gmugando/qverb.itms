17a0ecec680325a2a6cdfd6b34ee1d64c294f9fb
gmugando@gmail.com <gmugando@gmail.com>
2014-12-10 13:52:36 +0200

    Account

--- QverbITMS.Web/Controllers/AccountController.cs
+++ QverbITMS.Web/Controllers/AccountController.cs
@@ -80,7 +80,7 @@ namespace QverbITMS.Web.Controllers
                 // Attempt to register the user
                 try
                 {
-                    WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
+                    WebSecurity.CreateUserAndAccount(model.UserName, model.Password, new {Email= model.Email, Male = model.Male});
                     WebSecurity.Login(model.UserName, model.Password);
                     return RedirectToAction("Index", "Home");
                 }
