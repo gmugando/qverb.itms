f2cdc1b0b4c3917e39c61900f0efe34804b14cc0
gmugando@gmail.com <gmugando@gmail.com>
2014-12-22 17:20:07 +0200

    Task Class

--- QverbITMS.Core/Domain/Task.cs
+++ QverbITMS.Core/Domain/Task.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using QverbITMS.Core.Enums;
 
 namespace QverbITMS.Core.Domain
 {
@@ -9,5 +10,14 @@ namespace QverbITMS.Core.Domain
     {
         public string Name { get; set; }
         public string Descr { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public TaskCategory Category { get; set; }
+        public int ProjectId { get; set; }
+        public int AssignedTo { get; set; } // user
+        public bool Status { get; set; } // open or closed
+        public PriorityEnum Priority { get; set; }
+
+
     }
 }
