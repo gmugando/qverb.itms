22e905a11d3680dd3fed222cd71b75e7f0a0f832
gmugando@gmail.com <gmugando@gmail.com>
2014-12-22 10:51:59 +0200

    user profile

---
+++ QverbITMS.Core/IWorkContext.cs
@@ -0,0 +1,30 @@
+﻿using QverbITMS.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QverbITMS.Core
+{
+    /// <summary>
+    /// Work context
+    /// </summary>
+    public interface IWorkContext
+    {
+        /// <summary>
+        /// Gets or sets the current user
+        /// </summary>
+        SystemUserProfile CurrentUser { get; set; }
+
+        /// <summary>
+        /// Get or set value indicating whether we're in admin area
+        /// </summary>
+        bool IsAdmin { get; set; }
+
+        ///// <summary>
+        ///// Get or set a value indicating whether we're in the public area
+        ///// </summary>
+        //bool IsPublic { get; set; }
+    }
+}
